b6aff9f2e7a828c34eb0d0e1ed84b01e594ecee1
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-08-12 21:24:11 +0900

    エラーページ、投稿完了モーダル作成

--- js/main.js
+++ js/main.js
@@ -1,36 +1,37 @@
 'use strict';
 
 {
-  const modal = document.getElementById('modal')
+  const modal = document.getElementById('post-modal')
   const modalOpen = document.getElementById('modal-open');
   const spModalOpen = document.getElementById('sp-modal-open');
   const modalClose = document.getElementById('modal-close');
+  const postButton = document.getElementById('post-button');
+  const postInner = document.getElementById('post-modal-inner');
+  const postCompletion = document.getElementById('post-completion');
   const body = document.querySelector('body');
 
   // 記録・投稿ボタンクリック時にモーダルを開く
   modalOpen.addEventListener('click',()=> {
     console.log('aaa');
     console.log(body);
-    modal.classList.add('js-on');
-    body.classList.add('js-body');
+    modal.classList.add('js-block');
   })
 
+  // スマホの記録・投稿ボタンクリック時にモーダルを開く
   spModalOpen.addEventListener('click',()=> {
-    console.log('aaa');
-    console.log(body);
-    modal.classList.add('js-on');
-    body.classList.add('js-body');
+    modal.classList.add('js-block');
   })
 
   // モーダルのバツボタンを押したときにモーダルを閉じる
   modalClose.addEventListener('click',()=> {
-    modal.classList.remove('js-on');
+    modal.classList.remove('js-block');
+    postCompletion.classList.remove('js-flex')
+    postInner.classList.remove('js-off');
   })
 
-  addEventListener('click', outsideClose);
-function outsideClose(e) {
-  if (e.target == modal) {
-    modal.classList.remove('js-on');
-  }
-}
+  //
+  postButton.addEventListener('click',()=> {
+    postCompletion.classList.add('js-flex');
+    postInner.classList.add('js-off');
+  })
 }
