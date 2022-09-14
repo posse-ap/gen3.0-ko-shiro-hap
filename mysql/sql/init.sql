aebd1f067b93d58f7c43b9feeac022686a7dc2ea
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-09-14 17:04:37 +0900

    チャートの中身をデータベースの値から作成

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -15,6 +15,18 @@ CREATE TABLE records (
 
 INSERT INTO records(study_date, study_time, language_id, content_id)
 VALUES
+  ('2022-9-1', 2, 1, 1),
+  ('2022-9-2', 2, 1, 1),
+  ('2022-9-3', 2, 1, 1),
+  ('2022-9-4', 2, 1, 1),
+  ('2022-9-5', 2, 1, 1),
+  ('2022-9-6', 2, 1, 1),
+  ('2022-9-7', 2, 1, 1),
+  ('2022-9-8', 2, 1, 1),
+  ('2022-9-9', 2, 1, 1),
+  ('2022-9-10', 2, 1, 1),
+  ('2022-9-11', 2, 1, 1),
+  ('2022-9-12', 2, 1, 1),
   ('2022-9-13', 2, 1, 1),
   ('2022-9-14', 2, 1, 1),
   ('2022-9-15', 2, 1, 1),
@@ -29,13 +41,12 @@ VALUES
   ('2022-9-24', 3, 2, 3),
   ('2022-9-25', 2, 3, 3),
   ('2022-9-26', 3, 4, 3),
-  ('2022-4-27', 4, 1, 1),
-  ('2022-4-27', 3, 2, 1),
-  ('2022-4-27', 2, 3, 2),
-  ('2022-4-27', 3, 4, 2),
-  ('2022-5-27', 4, 1, 3),
-  ('2022-5-27', 3, 2, 3),
-  ('2022-5-27', 2, 3, 3);
+  ('2022-9-27', 4, 1, 1),
+  ('2022-9-27', 3, 2, 1),
+  ('2022-9-28', 3, 2, 3),
+  ('2022-9-29', 3, 2, 3),
+  ('2022-9-30', 3, 2, 3),
+  ('2022-9-31', 2, 3, 3);
 
 
 
@@ -44,32 +55,30 @@ VALUES
 DROP TABLE IF EXISTS languages;
 CREATE TABLE languages (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  language VARCHAR(255) NOT NULL,
-  colour VARCHAR(255) NOT NULL
+  language VARCHAR(255) NOT NULL
 );
 
-INSERT INTO languages(language, colour)
+INSERT INTO languages(language)
 VALUES
-  ('HTML', '#0445ec'),
-  ('CSS', '#0f70bd'),
-  ('Javascript', '#20bdde'),
-  ('PHP', '#3ccefe'),
-  ('Laravel', '#b29ef3'),
-  ('SQL', '#4a17ef'),
-  ('SHELL', '#3005c0'),
-  ('その他', '#6c46eb');
+  ('HTML'),
+  ('CSS'),
+  ('Javascript'),
+  ('PHP'),
+  ('Laravel'),
+  ('SQL'),
+  ('SHELL'),
+  ('その他');
 
 -- 学習コンテンツのテーブル
 
 DROP TABLE IF EXISTS contents;
 CREATE TABLE contents (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  content VARCHAR(255) NOT NULL,
-  colour VARCHAR(255) NOT NULL
+  content VARCHAR(255) NOT NULL
 );
 
-INSERT INTO contents(content, colour)
+INSERT INTO contents(content)
 VALUES
-  ('N予備校', '#0445ec'),
-  ('課題', '#0f70bd'),
-  ('ドットインストール', '#20bdde');
+  ('N予備校'),
+  ('課題'),
+  ('ドットインストール');
--- src/charts.php
+++ src/charts.php
@@ -1,5 +1,6 @@
 <?php
 
+// 棒グラフ用のデータ
 $bar_stmt = $pdo->query(
   "SELECT
     SUM(study_time)
@@ -11,11 +12,35 @@ $bar_stmt = $pdo->query(
     DATE_FORMAT(study_date, '%M/%Y') = DATE_FORMAT(now(), '%M/%Y')"
 );
 $bar = $bar_stmt->fetchALL();
-// print_r($bar);
+
+
+// 学習言語のデータ
+$languages_stmt = $pdo->query(
+  "SELECT
+    SUM(records.study_time), languages.language
+  FROM
+    records
+  JOIN
+    languages ON records.language_id = languages.id
+  GROUP BY
+    languages.language"
+    );
+$languages = $languages_stmt->fetchAll() ?: 0;
 
 
 
-
+// 学習コンテンツのデータ
+$contents_stmt = $pdo->query(
+  "SELECT
+    SUM(records.study_time), contents.content
+  FROM
+    records
+  JOIN
+    contents ON records.language_id = contents.id
+  GROUP BY
+    contents.content"
+    );
+$contents = $contents_stmt->fetchAll() ?: 0;
 
 
 
@@ -26,123 +51,137 @@ $bar = $bar_stmt->fetchALL();
 Chart.register(ChartDataLabels);
 
 {
-  // bar-chart-----------------------------------
+  // 棒グラフ-----------------------------------
 
-  const TIME_LOG_DATA_URL = 'http://posse-task.anti-pattern.co.jp/1st-work/study_time.json';
-  fetch(TIME_LOG_DATA_URL).then((response) => response.json()).then((jsonData) => {
-    // JSONデータを扱った処理
-    createTimeLogChart(jsonData);
-  })
+  const timeLogChart = document.getElementById('time-log-chart').getContext("2d");;
+  const gradient = timeLogChart.createLinearGradient(0, 20, 0, 100);
+  gradient.addColorStop(0, "#3ccfff");
+  gradient.addColorStop(1.0, "#0f71bc");
+  timeLogChart.fillStyle = gradient;
+  timeLogChart.fillRect(0, 0, 0, 0);
 
-  function createTimeLogChart(data) {
-    const dayData = data.map(function(e) {
-      return e.day;
-    })
+  // 棒グラフ作成
+  new Chart(timeLogChart, {
+    type: 'bar',
+    data: {
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+        28, 29, 30
+      ],
+      datasets: [{
+        data: [<?php
+          foreach ($bar as $bar_data) {
+            echo $bar_data['SUM(study_time)'] . ',';
+          }
+          ?>],
+        backgroundColor: gradient,
+        borderWidth: 2,
+        borderRadius: 10,
+      }],
+    },
+    options: {
+      responsive: true,
+      scales: {
 
-    const timeData = data.map(function(e) {
-      return e.time;
-    })
-
-    const timeLogChart = document.getElementById('time-log-chart').getContext("2d");;
-    const gradient = timeLogChart.createLinearGradient(0, 20, 0, 100);
-    gradient.addColorStop(0, "#3ccfff");
-    gradient.addColorStop(1.0, "#0f71bc");
-    timeLogChart.fillStyle = gradient;
-    timeLogChart.fillRect(0, 0, 0, 0);
-
-    // 棒グラフ作成
-    new Chart(timeLogChart, {
-      type: 'bar',
-      data: {
-        labels: dayData,
-        datasets: [{
-          data: timeData,
-          backgroundColor: gradient,
-          borderWidth: 2,
-          borderRadius: 10,
-        }],
+        x: {
+          grid: {
+            display: false,
+          },
+          ticks: {
+            min: 1,
+            max: 31,
+            stepSize: 2,
+            callback: function(value) {
+              if (value % 2 != 0 && value != 0) {
+                return value + 1;
+              };
+            }
+          },
+        },
+        y: {
+          grid: {
+            display: false,
+          },
+          ticks: {
+            stepSize: 2,
+            callback: function(value) {
+              return value + 'h';
+            }
+          },
+        }
       },
-      options: {
-        responsive: true,
-        scales: {
-
-          x: {
-            grid: {
-              display: false,
-            },
-            ticks: {
-              stepSize: 2,
-              callback: function(value) {
-                if (value % 2 != 0 && value != 0) {
-                  return value + 1;
-                };
-              }
-            },
-          },
-          y: {
-            grid: {
-              display: false,
-            },
-            ticks: {
-              stepSize: 2,
-              callback: function(value) {
-                return value + 'h';
-              }
-            },
-          }
+      plugins: {
+        legend: {
+          display: false
         },
-        plugins: {
-          legend: {
-            display: false
-          },
-          datalabels: {
-            display: false
-          }
-        },
-      }
-    })
-  }
-
-
-  // pie-chart-----------------------------------
-
-  // 学習言語のデータ取得
-  const LANGUAGE_DATA_URL = 'http://posse-task.anti-pattern.co.jp/1st-work/study_language.json';
-  fetch(LANGUAGE_DATA_URL).then((response) => response.json()).then((jsonData) => {
-    // JSONデータを扱った処理
-    createLanguagesChart(jsonData);
+        datalabels: {
+          display: false
+        }
+      },
+    }
   })
-
-  // 学習コンテンツのデータ取得
-  const CONTENTS_DATA_URL = 'http://posse-task.anti-pattern.co.jp/1st-work/study_contents.json';
-  fetch(CONTENTS_DATA_URL).then((response) => response.json()).then((jsonData) => {
-    // JSONデータを扱った処理
-    createContentsChart(jsonData);
-  })
+  // }
 
 
+  // 円グラフ-----------------------------------
 
   const bgColor = [
-    "#0000ff", "#4682b4", "#40e0d0", "#87ceeb", "#dda0dd", "#9400d3", "#0000cd", "#00008b"
+    "#0445ec", "#0f70bd", "#20bdde", "#3ccefe", "#b29ef3", "#4a17ef", "#3005c0", "#6c46eb"
   ]
 
   // 学習言語のドーナツチャート作成
-  function createLanguagesChart(data) {
-    const languagesChart = document.getElementById('languages-chart');
-    const languagesLabels = Object.keys(data[0]);
-    const languagesPercent = Object.values(data[0]);
 
-    createDoughnutChart(languagesChart, languagesLabels, languagesPercent, bgColor)
+  const languagesChart = document.getElementById('languages-chart');
+  // ラベルの配列を作成
+  const languagesLabels = [<?php
+    foreach ($languages as $language) {
+      echo "'" . $language['language'] . "'" . ',';
+    }
+  ?>];
+
+  // 100分率で表示するための、言語学習の合計時間を格納する
+  <?php
+  $sumLanguagesRecords = 0;
+  foreach ($languages as $language) {
+    $sumLanguagesRecords+= $language['SUM(records.study_time)'];
   }
+  ?>
+
+  // 言語の学習時間の配列を作成、学習時間を百分率に変換
+  const languagesRecords = [<?php
+    foreach ($languages as $language) {
+      echo intval($language['SUM(records.study_time)']/$sumLanguagesRecords*100) . ',';
+    }
+  ?>];
+
+  createDoughnutChart(languagesChart, languagesLabels, languagesRecords, bgColor)
+
 
   // 学習コンテンツのドーナツチャート作成
-  function createContentsChart(data) {
-    const contentsChart = document.getElementById('contents-chart');
-    const contentsLabels = Object.keys(data[0])
-    const contentsPercent = Object.values(data[0])
 
-    createDoughnutChart(contentsChart, contentsLabels, contentsPercent, bgColor)
+  const contentsChart = document.getElementById('contents-chart');
+  // ラベルの配列を作成
+  const contentsLabels = [<?php
+    foreach ($contents as $content) {
+      echo "'" . $content['content'] . "'" . ',';
+    }
+  ?>];
+
+  // 100分率で表示するための、コンテンツ学習の合計時間を格納する
+  <?php
+  $sumContentsRecords = 0;
+  foreach ($contents as $content) {
+    $sumContentsRecords+= $content['SUM(records.study_time)'];
   }
+  ?>
+
+  // コンテンツの学習時間の配列を作成、学習時間を百分率に変換
+  const contentsRecords = [<?php
+    foreach ($contents as $content) {
+      echo intval($content['SUM(records.study_time)']/$sumContentsRecords*100) . ',';
+    }
+  ?>];
+
+  createDoughnutChart(contentsChart, contentsLabels, contentsRecords, bgColor)
 
 
   // 円グラフ作成
