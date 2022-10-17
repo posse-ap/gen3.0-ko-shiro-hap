0f6ba5ab37a9f597c92392dd3a517ec2baa64541
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-10-17 20:22:22 +0900

    メールの設定を修正、userTableの初期値を空にした

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RecordsTableSeeder::class);
         $this->call(LanguagesTableSeeder::class);
         $this->call(ContentsTableSeeder::class);
-        $this->call(UserTableSeeder::class);
+        // $this->call(UserTableSeeder::class);
     }
 }
