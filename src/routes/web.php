8123a8fb94ae962f6314e2d0fd65fd97d8a3bcea
ko-shiro-hap <koushirou.iino@icloud.com>
2023-07-01 22:58:26 +0900

    add: ユーザー一覧を作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -16,18 +17,12 @@ use Inertia\Inertia;
 |
 */
 
-
+// top
 Route::get('/', function () {
     return Inertia::render('Top');
 });
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
+// users-list
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
